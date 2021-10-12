540da1de0fdf456e4a73c1df914c5a41b04a3a49
William Watson <williampwatson1991@gmail.com>
2021-10-12 16:22:41 -0400

    feat: terminal event created using stdin emitter.emit

--- events.js
+++ events.js
@@ -6,5 +6,14 @@ emitter.on('customEvent', (message, user) => {
   console.log(`${user}: ${message}`);
 });
 
-emitter.emit('customEvent', 'Hello World', 'Computer');
-emitter.emit('customEvent', 'Thats pretty cool', 'Will');
+// emitter.emit('customEvent', 'Hello World', 'Computer');
+// emitter.emit('customEvent', 'Thats pretty cool', 'Will');
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  if (input === 'exit') {
+    emitter.emit('customEvent', 'GoodBye', 'Process');
+    process.exit();
+  }
+  emitter.emit('customEvent', input, 'terminal');
+});
