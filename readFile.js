a43198ebd5afdc1ab0c9ffef6e78b137d1ef3484
William Watson <williampwatson1991@gmail.com>
2021-10-12 16:51:59 -0400

    feat: error handling

--- readFile.js
+++ readFile.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
 //const text = fs.readFileSync('./lib/collectAnswers.js', 'UTF-8');
+
+//if it were an image, or binary, file you wouldnt have to specifiy UTF-8
+
 const text = fs.readFile('./lib/collectAnswers.js', 'UTF-8', (err, text) => {
+  if (err) {
+    console.log(`an error has occured: ${err.message}`);
+    process.exit();
+  }
+
   console.log('file contents read');
   console.log(text);
 });
