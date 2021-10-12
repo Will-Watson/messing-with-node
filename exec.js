22edb07bd8467a6b6d9760c204baaeea03f6c97e
William Watson <williampwatson1991@gmail.com>
2021-10-12 17:43:33 -0400

    feat: messing with exec/cp

---
+++ exec.js
@@ -0,0 +1,12 @@
+const cp = require('child_process');
+
+//cp.exec('open http://www.linkedin.com/learning');
+
+//cp.exec('open -a Terminal .');
+
+cp.exec('ls', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
