ee28c3dfc7bdd04d8c7867e4c399f5c4bf4e1351
William Watson <williampwatson1991@gmail.com>
2021-10-12 16:00:23 -0400

    feat: basic questions application created using node.js and the command line

--- questions.js
+++ questions.js
@@ -1,5 +1,10 @@
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const questions = [
   'What is your name? ',
   'What is your favorite programming language? ',
@@ -9,5 +14,22 @@ const questions = [
 
 const collectAnswers = (questions, done) => {
   const answers = [];
-  //done(answers);
+  const [firstQuestion] = questions;
+
+  const questionAnswered = (answer) => {
+    answers.push(answer);
+    if (answers.length < questions.length) {
+      rl.question(questions[answers.length], questionAnswered);
+    } else {
+      done(answers);
+    }
+  };
+
+  rl.question(firstQuestion, questionAnswered);
 };
+
+collectAnswers(questions, (answers) => {
+  console.log('Thank you for your answers; ');
+  console.log(answers);
+  process.exit();
+});
