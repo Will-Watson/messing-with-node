9042964695d408620067c45214c90e6b76d1f1b3
William Watson <williampwatson1991@gmail.com>
2021-10-12 16:04:04 -0400

    feat: messing with modules file created. Dummy count functions created for exportring purposes

---
+++ app.js
@@ -0,0 +1,3 @@
+const name = require('./myModule');
+
+//onsole.log(name);
---
+++ myModule.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+const inc = () => ++count;
+const dec = () => --count;
+
+const getCount = () => {
+  return count;
+};
+
+module.exports = {
+  inc,
+  dec,
+  getCount,
+};
