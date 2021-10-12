862ee0ac7a545e14ce47b1eb270bbbd042930f8e
William Watson <williampwatson1991@gmail.com>
2021-10-12 15:52:35 -0400

    feat: dummy function for answer collection created

--- questions.js
+++ questions.js
@@ -3,14 +3,11 @@ const readline = require('readline');
 const questions = [
   'What is your name? ',
   'What is your favorite programming language? ',
+  'Where do you live?',
   'Who is your daddy and what does he do? ',
 ];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.questions('how was your day? ', (answer) => {
-  console.log(`Your anser: ${answer}`);
-});
+const collectAnswers = (questions, done) => {
+  const answers = [];
+  //done(answers);
+};
