ae47fa4021695e8c576b9341b410ab6ee244b82f
William Watson <williampwatson1991@gmail.com>
2021-10-12 17:45:16 -0400

    feat: can use exec to call other files as well

--- exec.js
+++ exec.js
@@ -4,7 +4,14 @@ const cp = require('child_process');
 
 //cp.exec('open -a Terminal .');
 
-cp.exec('ls', (err, data) => {
+// cp.exec('ls', (err, data, stderr) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
+
+cp.exec('node readStream', (err, data, stderr) => {
   if (err) {
     throw err;
   }
