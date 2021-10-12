9e3d57edb2142d24d5e41d5ea80236d3d7e3c40b
William Watson <williampwatson1991@gmail.com>
2021-10-12 16:50:37 -0400

    feat: readFile without Sync

--- readFile.js
+++ readFile.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
-const text = fs.readFileSync('./lib/collectAnswers.js', 'UTF-8');
+//const text = fs.readFileSync('./lib/collectAnswers.js', 'UTF-8');
+const text = fs.readFile('./lib/collectAnswers.js', 'UTF-8', (err, text) => {
+  console.log('file contents read');
+  console.log(text);
+});
 
-console.log(text);
+//console.log(text);
