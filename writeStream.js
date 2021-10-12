6c4c8abf64a2b5261ca296e7f1438b7f4574f572
William Watson <williampwatson1991@gmail.com>
2021-10-12 17:37:01 -0400

    feat: writing random text to file

--- writeStream.js
+++ writeStream.js
@@ -5,5 +5,9 @@ const writeStream = fs.createWriteStream('./lib/myFile.txt', 'UTF-8');
 // process.stdout.write('hello ');
 // process.stdout.write('world \n');
 
-writeStream.write('Hello ');
-writeStream.write('World \n');
+// writeStream.write('Hello ');
+// writeStream.write('World \n');
+
+process.stdin.on('data', (data) => {
+  writeStream.write(data);
+});
