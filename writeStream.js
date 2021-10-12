6ff3dbc564404bc57526371ca562692016f97c76
William Watson <williampwatson1991@gmail.com>
2021-10-12 17:39:41 -0400

    feat: reading and writing streams in unison and added at the end using pipe

--- writeStream.js
+++ writeStream.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const writeStream = fs.createWriteStream('./lib/myFile.txt', 'UTF-8');
+const readStream = fs.createReadStream('./lib/lorem-ipsum.md', 'UTF-8');
 
 // process.stdout.write('hello ');
 // process.stdout.write('world \n');
@@ -8,6 +9,11 @@ const writeStream = fs.createWriteStream('./lib/myFile.txt', 'UTF-8');
 // writeStream.write('Hello ');
 // writeStream.write('World \n');
 
-process.stdin.on('data', (data) => {
-  writeStream.write(data);
-});
+// readStream.on('data', (data) => {
+//   writeStream.write(data);
+// });
+// process.stdin.on('data', (data) => {
+//   writeStream.write(data);
+// });
+
+process.stdin.pipe(writeStream);
