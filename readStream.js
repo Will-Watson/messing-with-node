1d5253440edd102206c35a12abae56f8ba6e838c
William Watson <williampwatson1991@gmail.com>
2021-10-12 17:28:45 -0400

    feat: readStream is reading the amount of characters in the lorem ipsum

--- readStream.js
+++ readStream.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
+const readStream = fs.createReadStream('./lib/lorem-ipsum.md', 'UTF-8');
+
 console.log('type something.... ');
-process.stdin.on('data', (data) => {
+readStream.on('data', (data) => {
   console.log(`I read ${data.length - 1} characters of text`);
 });
+// console.log('type something.... ');
+// process.stdin.on('data', (data) => {
+//   console.log(`I read ${data.length - 1} characters of text`);
+// });
